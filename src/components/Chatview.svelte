2d2200bd4d2ec3725035d24e1edaee3357d92ced
auraecosystem <sandraisom@icloud.com>
2026-05-29 20:36:04 +0100

    Create Chatview.svelte

---
+++ src/components/Chatview.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { messages } from '../stores/messages'
+</script>
+
+<div class="chat-view">
+  {#each $messages as message}
+    <div class="message">
+      {message}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .chat-view {
+    flex: 1;
+    overflow-y: auto;
+    padding: 20px;
+  }
+
+  .message {
+    background: #18213c;
+    padding: 14px;
+    border-radius: 12px;
+    margin-bottom: 10px;
+  }
+</style>
