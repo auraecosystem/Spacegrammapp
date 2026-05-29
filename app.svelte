41b38a81630e912c13c9965d159b4a142a7bbddb
auraecosystem <sandraisom@icloud.com>
2026-05-29 20:52:37 +0100

    Create app.svelte

---
+++ app.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { sendMessage } from "./lib/api";
+
+  let input = "";
+  let log: string[] = [];
+
+  async function send() {
+    const res = await sendMessage("user", input);
+    log = [...log, res];
+    input = "";
+  }
+</script>
+
+<main>
+  <h1>Spacegramm</h1>
+
+  <input bind:value={input} placeholder="Type message..." />
+  <button on:click={send}>Send</button>
+
+  <div>
+    {#each log as item}
+      <p>{item}</p>
+    {/each}
+  </div>
+</main>
+
+<style>
+  main {
+    font-family: sans-serif;
+    padding: 20px;
+  }
+</style>
