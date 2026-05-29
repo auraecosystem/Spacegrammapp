70f98b5611771e470aa971cd6a4ee591a20a6f2b
auraecosystem <sandraisom@icloud.com>
2026-05-29 20:36:37 +0100

    Create sidebar.svelte

---
+++ src/components/sidebar.svelte
@@ -0,0 +1,22 @@
+<div class="sidebar">
+  <h2>Spacegramm</h2>
+
+  <div class="chat">General</div>
+  <div class="chat">Web4</div>
+  <div class="chat">AI Nodes</div>
+</div>
+
+<style>
+  .sidebar {
+    width: 260px;
+    background: #11182d;
+    padding: 20px;
+  }
+
+  .chat {
+    padding: 12px;
+    background: #18213c;
+    margin-top: 10px;
+    border-radius: 10px;
+  }
+</style>
