bafdf20e182c9e12ead03eb6044e7689d6f9a37a
auraecosystem <sandraisom@icloud.com>
2026-05-29 22:06:59 +0100

    Update Main.ts

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,11 @@
-import App from "./app.svelte";
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
 
-const app = new App({
-  target: document.getElementById("app")!
-});
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
 
-export default app;
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
+}
