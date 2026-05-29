63b4367cea376fb0b39703208ea7b7381a8592db
auraecosystem <sandraisom@icloud.com>
2026-05-29 20:37:04 +0100

    Create app.svelte

---
+++ src/app.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Sidebar from './components/Sidebar.svelte'
+  import ChatView from './components/ChatView.svelte'
+  import MessageInput from './components/MessageInput.svelte'
+</script>
+
+<div class="app">
+  <Sidebar />
+
+  <div class="chat-area">
+    <ChatView />
+    <MessageInput />
+  </div>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    background: #0b1020;
+    color: white;
+    font-family: sans-serif;
+  }
+
+  .app {
+    display: flex;
+    height: 100vh;
+  }
+
+  .chat-area {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
