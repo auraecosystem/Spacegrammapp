609e0a2b7bbabacda8b4bf5ccb0b2d48a53e35a1
auraecosystem <sandraisom@icloud.com>
2026-05-30 03:43:27 +0100

    Update and rename telegram-webview.js to tauri-src/telegram-webview.js

--- tauri-src/telegram-webview.js
+++ tauri-src/telegram-webview.js
@@ -1,17 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-import "./telegram-webview.js";
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
 // WebView 
 (function () {
   var eventHandlers = {};
